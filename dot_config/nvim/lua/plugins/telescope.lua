6dbb138dc279ccbcb2d95051cc5692a037f82c8b
Nash Sauter <nashsauter1@gmail.com>
2024-05-20 06:33:47 -0600

    nvim restart

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,74 +1,9 @@
 return {
-
-  { "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "natecraddock/telescope-zf-native.nvim",
-      "nvim-telescope/telescope-bibtex.nvim"
-    },
-    config = function()
-      require('telescope').setup {
-        defaults = {
-          -- Default configuration for telescope goes here:
-          -- config_key = value,
-          mappings = {
-            i = {
-              -- map actions.which_key to <C-h> (default: <C-/>)
-              -- actions.which_key shows the mappings for your picker,
-              -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-              ["<C-h>"] = "which_key"
-            }
-          }
-        },
-        pickers = {
-          -- Default configuration for builtin pickers goes here:
-          -- picker_name = {
-          --   picker_config_key = value,
-          --   ...
-          -- }
-          -- Now the picker_config_key will be applied every time you call this
-          -- builtin picker
-        },
-        extensions = {
-          -- Your extension configuration goes here:
-          -- extension_name = {
-          --   extension_config_key = value,
-          -- }
-          -- please take a look at the readme of the extension you want to configure
-          ["zf-native"] = {
-            -- options for sorting file-like items
-            file = {
-              -- override default telescope file sorter
-              enable = true,
-
-              -- highlight matching text in results
-              highlight_results = true,
-
-              -- enable zf filename match priority
-              match_filename = true,
-            },
-
-            -- options for sorting all other items
-            generic = {
-              -- override default telescope generic item sorter
-              enable = true,
-
-              -- highlight matching text in results
-              highlight_results = true,
-
-              -- disable zf filename match priority
-              match_filename = false,
-            },
-          },
-          ["bibtex"] = {
-            global_files = { "/Users/nashsauter/Documents/library.bib" },
-            context = true,
-            context_fallback = true,
-          },
-        },
-      }
-      require "telescope".load_extension("bibtex")
-    end
-
-  }
-
+  'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = 'Files' })
+    vim.keymap.set('n', '<leader>tv', builtin.vim_options, { desc = 'Vim Options' })
+  end,
 }
